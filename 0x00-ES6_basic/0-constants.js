0bfffd3fa11741672dde617aa047fdca4209dc96
simonkari <kariithisimon1715@gmail.com>
2023-11-27 21:30:09 +0300

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,34 +1,15 @@
-const counter = 0;
-let task = 'I prefer const when I can.';
-
-function taskFirst() {
- task = 'Study different data structures';
- return task;
-}
-
-function getLast() {
- return counter;
-}
-
-function taskNext() {
- let combination = 'But sometimes let';
- combination += getLast();
-
- return combination;
-}
-
 export function taskFirst() {
- var task = 'I prefer const when I can.';
- return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
 export function getLast() {
- return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
- var combination = 'But sometimes let';
- combination += getLast();
+  let combination = 'But sometimes let';
+  combination += getLast();
 
- return combination;
+  return combination;
 }
