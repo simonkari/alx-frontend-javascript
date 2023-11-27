e3958ee4c667a82596bb767045808de85fbad4ce
simonkari <kariithisimon1715@gmail.com>
2023-11-27 20:46:27 +0300

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,18 @@
-export function taskFirst() {
+function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
-}
-
-export function getLast() {
+ }
+ 
+ function getLast() {
   return ' is okay';
-}
-
-export function taskNext() {
+ }
+ 
+ function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
-
+ 
   return combination;
-}
+ }
+ 
+ export { taskFirst, taskNext };
+ 
