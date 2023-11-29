10f64fc9a58f941f284d8850bc7faa850a63b3ab
simonkari <kariithisimon1715@gmail.com>
2023-11-29 19:15:20 +0300

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,19 @@
+/**
+ * A function that simulates making a request to an API and returns a Promise.
+ *
+ * @returns {Promise}
+ */
 export default function getResponseFromAPI() {
+  // Simulate a successful API response
   const success = true;
+
+  // Create a new Promise that either resolves or rejects based on the 'success' variable
   return new Promise((resolve, reject) => {
     if (success) {
+      // Resolve the Promise if the API request is successful
       resolve();
     } else {
+      // Reject the Promise if the API request fails
       reject();
     }
   });
