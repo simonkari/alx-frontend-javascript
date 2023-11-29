338d457c15bbf4ebb7a3d01f457b7324d381efc3
simonkari <kariithisimon1715@gmail.com>
2023-11-29 19:40:47 +0300

    4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,4 +1,17 @@
+/**
+ * Simulates signing up a user by creating a user object
+ * with the provided first name and last name.
+ *
+ * The first name of the user.
+ * @param {string} firstName
+ * The last name of the user.
+ * @param {string} lastName
+ * A Promise that resolves with an object containing the
+ * user's first name and last name.
+ * @returns {Promise}
+ */
 export default function signUpUser(firstName, lastName) {
+  // Create a Promise that immediately resolves with a user object
   return Promise.resolve({
     firstName: `${firstName}`,
     lastName: `${lastName}`,
