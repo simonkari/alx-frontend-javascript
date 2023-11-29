f0fc95ae7ae7a32c8e436b2e66c9cb9f2c774de8
simonkari <kariithisimon1715@gmail.com>
2023-11-29 20:07:37 +0300

    100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,26 @@
 import { uploadPhoto, createUser } from './utils';
 
+/**
+ * Asynchronously uploads a photo and creates a user,
+ * returning an object with the photo and user information.
+ * If any step fails, it returns an object with null
+ * values for both photo and user.
+ *
+ * A Promise that resolves with an object containing
+ * the photo and user information, or rejects with an
+ * object containing null values if any step fails.
+ * @returns {Promise<Object>}
+ */
 const asyncUploadUser = async () => {
   try {
+    // Await the uploadPhoto and createUser functions to execute asynchronously
     const photo = await uploadPhoto();
     const user = await createUser();
 
+    // Return an object with the photo and user information
     return { photo, user };
   } catch (error) {
+    // If any step fails, return an object with null values for both photo and user
     return { photo: null, user: null };
   }
 };
