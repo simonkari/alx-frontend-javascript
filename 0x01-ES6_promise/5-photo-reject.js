beef0a1dde2a02d4449e1eddc6bb0d259d1bdf36
simonkari <kariithisimon1715@gmail.com>
2023-11-29 19:43:41 +0300

    5-photo-reject.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,14 @@
+/**
+ * Simulates uploading a photo with the provided filename.
+ * This function always rejects the Promise with an error
+ * indicating that the photo cannot be processed.
+ *
+ * The name of the photo file to be uploaded.
+ * @param {string} filename
+ * A Promise that always rejects with an error message.
+ * @returns {Promise}
+ */
 export default function uploadPhoto(filename) {
+  // Create a Promise that always rejects with an error indicating the photo cannot be processed
   return Promise.reject(Error(`${filename} cannot be processed`));
 }
