5056818acddb35d3e1c2bebf8aebc6b6980077cf
simonkari <kariithisimon1715@gmail.com>
2023-11-29 19:24:47 +0300

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,12 @@
+/**
+ * A function that handles a response from an API Promise and transforms it into a formatted object.
+ * It also logs a message indicating that a response has been received from the API, regardless of success or failure.
+ *
+ * The Promise representing the response from the API.
+ * @param {Promise} promise
+ * A Promise that resolves with a formatted success object or rejects with an error.
+ * @returns {Promise}
+ */
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ status: 200, body: 'success' }))
