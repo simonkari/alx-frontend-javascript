6151ad9967ad61f74539ee5de22fb664cd04f352
simonkari <kariithisimon1715@gmail.com>
2023-11-29 19:56:43 +0300

    7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,18 @@
+/**
+ * Simulates a load balancer by initiating download
+ * requests to servers in China and the US,
+ * and returns a Promise that resolves or rejects
+ * based on the first completed download.
+ *
+ * A Promise representing a download request to a server in China.
+ * @param {Promise} chinaDownload
+ * A Promise representing a download request to a server in the US.
+ * @param {Promise} USDownload
+ * A Promise that resolves or rejects based on the first completed download,
+ * providing the result or error of the faster download.
+ * @returns {Promise}
+ */
 export default function loadBalancer(chinaDownload, USDownload) {
+  // Use Promise.race to resolve or reject based on the first completed download
   return Promise.race([chinaDownload, USDownload]);
 }
