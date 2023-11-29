c16ac51a0823aac77dfba940875c6df9259ae542
simonkari <kariithisimon1715@gmail.com>
2023-11-29 19:18:28 +0300

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,23 @@
-export default function (boolean) {
+/**
+ * A function that simulates making a request to a fake API and returns a Promise.
+ *
+ * @param {boolean} boolean
+ * @returns {Promise}
+ */
+export default function fakeAPIRequest(boolean) {
+  // Create a new Promise that either resolves or rejects based on the 'boolean' parameter
   return new Promise((resolve, reject) => {
+    // Define a mock response object for a successful API request
     const object = {
       status: 200,
       body: 'Success',
     };
 
     if (boolean === true) {
+      // Resolve the Promise with the mock response object if the API request is successful
       resolve(object);
     } else {
+      // Reject the Promise with an error message if the API request fails
       reject(Error('The fake API is not working currently'));
     }
   });
