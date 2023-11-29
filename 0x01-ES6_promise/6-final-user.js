c22bc1adcb09c4b21d6dcc528f451da4f4df4298
simonkari <kariithisimon1715@gmail.com>
2023-11-29 19:51:10 +0300

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,22 +1,39 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
+/**
+ * Handles the process of signing up a user and uploading
+ * a photo, resolving or rejecting
+ * each operation independently. Returns an array of
+ * objects representing the status and value
+ * of each Promise, whether fulfilled or rejected.
+ *
+ * The first name of the user.
+ * @param {string} firstName
+ * The last name of the user.
+ * @param {string} lastName
+ * The name of the photo file to be uploaded.
+ * @param {string} fileName
+ * A Promise that resolves with an array of objects, each containing the status
+ * ('fulfilled' or 'rejected') and the value or error message of each operation.
+ * @returns {Promise} 
+ */
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  // Create Promises for signing up a user and uploading a photo
   const signUp = signUpUser(firstName, lastName);
   const upload = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUp, upload]).then((values) => {
-    const result = [];
-    values.forEach((val) => {
-      if (val.status === 'fulfilled') {
-        result.push({ status: val.status, value: val.value });
+  // Use Promise.allSettled to handle both promises independently
+  return Promise.allSettled([signUp, upload]).then((settledPromises) => {
+    // Process the results of each settled Promise and create an array of status and value/error objects
+    const result = settledPromises.map((settledPromise) => {
+      if (settledPromise.status === 'fulfilled') {
+        return { status: settledPromise.status, value: settledPromise.value };
       } else {
-        result.push({
-          status: val.status,
-          value: `Error: ${val.reason.message}`,
-        });
+        return { status: settledPromise.status, value: `Error: ${settledPromise.reason.message}` };
       }
     });
+
     return result;
   });
 }
