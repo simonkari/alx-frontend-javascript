a7d0e8d0c0b052d85094cc4ac82fe923118aebea
simonkari <kariithisimon1715@gmail.com>
2023-11-30 22:11:12 +0300

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,25 +1,10 @@
 import Car from './10-car';
 
-/**
- * Represents an electric vehicle (EV)
- * car with extended properties such as range.
- * Extends the Car class.
- */
-
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
-
-   /**
-   * Overrides the cloneCar method from the parent 
-   * class (Car).
-   * Creates a clone of the current EVCar instance 
-   * using the constructor specified by Symbol.species.
-   *
-   * @returns {EVCar} A new instance of the EVCar class.
-   */
 
   // eslint-disable-next-line
 	cloneCar() {
