796df9dce9eaeb9c0f02efa64b5a1b62a5f84609
simonkari <kariithisimon1715@gmail.com>
2023-11-30 21:27:06 +0300

    0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,20 @@
+/**
+ * Represents a classroom with a maximum capacity for students.
+ */
 export default class ClassRoom {
+  /**
+   * Creates a new instance of the ClassRoom class with the
+   * specified maximum number of students.
+   *
+   * The maximum capacity of the classroom for students.
+   * @param {number} maxStudentsSize
+   */
   constructor(maxStudentsSize) {
+    /**
+     * The maximum capacity of the classroom for students.
+     * @type {number}
+     * @private
+     */
     this._maxStudentsSize = maxStudentsSize;
   }
 }
