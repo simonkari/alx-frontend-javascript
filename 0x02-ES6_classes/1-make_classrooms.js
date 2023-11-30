d319f7889053f326fe18c61b6d6a5daf6bd1e3f0
simonkari <kariithisimon1715@gmail.com>
2023-11-30 21:33:43 +0300

    1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,19 +1,6 @@
 import ClassRoom from './0-classroom';
 
-/**
- * Initializes an array of ClassRoom instances with predefined
- * maximum student capacities.
- *
- * An array of ClassRoom instances with different maximum
- * student capacities.
- * @returns {Array}
- */
 export default function initializeRooms() {
-  /**
-   * An array of ClassRoom instances representing different classrooms.
-   * @type {Array}
-   */
   const arr = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
-  
   return arr;
 }
