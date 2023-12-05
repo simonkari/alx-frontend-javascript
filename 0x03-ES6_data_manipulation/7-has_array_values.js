67e7f9eca51e0a2fafebb138b46b03c7d4fbd2dd
simonkari <kariithisimon1715@gmail.com>
2023-12-05 21:17:56 +0300

    7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,4 @@
 export default function hasValuesFromArray(set, arr) {
+  // Use the reduce function to check if all values from the array are present in the Set.
   return arr.reduce((prev, curr) => prev && set.has(curr), true);
 }
