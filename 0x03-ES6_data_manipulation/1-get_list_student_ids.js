5dc1100217fca9a5beb9103cfbe5eaac4f6fdc7b
simonkari <kariithisimon1715@gmail.com>
2023-12-05 20:57:56 +0300

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,10 @@
 export default function getListStudentIds(lst) {
+  // Check if the input is an array.
   if (Array.isArray(lst)) {
+    // Use the map function to extract the 'id' property from each student object.
     return lst.map((obj) => obj.id);
   }
 
+  // Return an empty array if the input is not an array.
   return [];
 }
