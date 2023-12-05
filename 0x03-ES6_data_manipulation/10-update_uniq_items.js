4a7b16484597e92f44cab80aaa772c8661c1b4dc
simonkari <kariithisimon1715@gmail.com>
2023-12-05 21:35:01 +0300

    10-update_uniq_items.js

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,12 +1,19 @@
 export default function updateUniqueItems(map) {
+  // Check if the input is a Map.
   if (map instanceof Map) {
+    // Iterate through each key-value pair in the Map.
     for (const [key, value] of map) {
+      // Check if the value is 1 (indicating a unique item).
       if (value === 1) {
+        // Update the value to a specific value (e.g., 100).
         map.set(key, 100);
       }
     }
   } else {
-    throw new Error('Cannot process');
+    // Throw an error if the input is not a Map.
+    throw new Error('Cannot process. Input must be a Map.');
   }
+
+  // Return the updated Map.
   return map;
 }
