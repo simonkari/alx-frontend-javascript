588123d5855fa72b30abe4c2d66706c26dc0dfdd
simonkari <kariithisimon1715@gmail.com>
2023-12-05 21:26:13 +0300

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,19 +2,13 @@ export default function cleanSet(set, startString) {
   let text = '';
   const array = [];
 
-  // Check if startString is provided and is a valid string.
   if (startString && typeof startString === 'string') {
-    // Iterate through each element in the Set.
     for (const element of set) {
-      // Check if the element starts with the specified startString.
       if (element && element.startsWith(startString)) {
-        // Push the cleaned element (without startString) into the array.
         array.push(element.slice(startString.length));
       }
     }
-     // Join the cleaned elements using '-' as a separator to form the final text result.
     text = array.join('-');
   }
-  // Return the formatted text result.
   return text;
 }
