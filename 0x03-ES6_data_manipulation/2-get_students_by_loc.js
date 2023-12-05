15f3488027e728e19f7c9b2bd673eb20823ee504
simonkari <kariithisimon1715@gmail.com>
2023-12-05 21:00:23 +0300

    2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,5 @@
 export default function getStudentsByLocation(lst, city) {
+  // Use the filter function to select students whose
+  //location matches the specified city.
   return lst.filter((obj) => obj.location === city);
 }
