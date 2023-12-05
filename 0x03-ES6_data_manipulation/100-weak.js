c3b10d5fa308880222e97bd79d412da153356d13
simonkari <kariithisimon1715@gmail.com>
2023-12-05 21:37:57 +0300

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,9 +2,16 @@ export const weakMap = new WeakMap();
 let count = 1;
 
 export function queryAPI(endPoint) {
+  // Set or update the query count for the specified endpoint in the WeakMap.
   weakMap.set(endPoint, count);
+  
+  // Increment the global query count.
   count += 1;
+
+  // Get the current query count for the endpoint.
   const queryCount = weakMap.get(endPoint);
+
+  // Check if the query count exceeds the threshold (5).
   if (queryCount >= 5) {
     throw new Error('Endpoint load is high');
   }
