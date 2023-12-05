ad53c2dd54175d9a800144c71b516481368b97e9
simonkari <kariithisimon1715@gmail.com>
2023-12-05 21:04:49 +0300

    3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,4 @@
 export default function getStudentIdsSum(lst) {
+  // Use the reduce function to accumulate the sum of student IDs.
   return lst.reduce((counter, currentObj) => counter + currentObj.id, 0);
 }
