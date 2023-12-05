07a1cdeb4273f2948077afffc4da5f1facf7371a
simonkari <kariithisimon1715@gmail.com>
2023-12-05 21:08:18 +0300

    4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,4 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
+  // Filter students in the specified city and update their grades based on newGrades.
   return students.filter((std) => std.location === city).map((std) => ({ ...std, grade: newGrades.filter((g) => g.studentId === std.id).map((g) => g.grade)[0] || 'N/A' }));
 }
