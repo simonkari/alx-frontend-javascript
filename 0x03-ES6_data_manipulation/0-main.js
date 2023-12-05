a763b3f8ed07dac05492c980d7b917373e0ad048
simonkari <kariithisimon1715@gmail.com>
2023-12-05 20:55:08 +0300

    0-main.js

--- 0x03-ES6_data_manipulation/0-main.js
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -1,3 +1,10 @@
+// File: YourCurrentFile.js
+// This script imports the getListStudents function
+//and logs the result to the console.
+
+// Import the getListStudents function from the module.
 import getListStudents from "./0-get_list_students.js";
 
+// Call the getListStudents function and log the result
+//to the console.
 console.log(getListStudents());
